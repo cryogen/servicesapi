b32dad87e36f2d790b30301add772d13e13b659b
Stuart Walsh <stu@ipng.org.uk>
2015-07-09 10:18:45 +0100

    redirect to index on logout

--- client/scripts/controllers/account/logout.js
+++ client/scripts/controllers/account/logout.js
@@ -1,10 +1,11 @@
 /*global angular */
 'use strict';
 
-angular.module('services').controller('AccountLogoutController', function($scope, $localStorage) {
+angular.module('services').controller('AccountLogoutController', function($scope, $localStorage, $location) {
     $scope.logout = function() {
         delete $localStorage.token;
         $scope.$emit('tokenChanged', $localStorage.token);
+        $location.path('/');
     };
 
     $scope.logout();
